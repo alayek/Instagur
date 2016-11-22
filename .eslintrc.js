0730877e47a4f2e65c8b93463634966d86a14a14
alayek <alayek@engineer.com>
2016-11-23 01:06:30 +0530

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "installedESLint": true,
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+      "node": true,
+      "mocha": true
+    }
+};
